fe3145dfef2c3deb07baf5b73679cbfd03ff165f
John Philip Limpahan <jlimpahan@ad.acquireap>
2018-03-26 20:39:11 +1100

    Fixed some margins

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,9 +1,9 @@
 
 <section id="contact-section" class ="bg-color-grey py-5 contact-height">
-    <header>
-        <h2 class="title text-center pt-2 pb-3" style="letter-spacing: 4.5px; padding-left: 4.7%;">GIVE US A CALL: <span class="d-xs-block d-sm-block d-md-block d-lg-inline title-header text-center"style="padding-top: 10px;">1-800-972-3981</span></h2>
-    </header>
         <div class="col-sm-10 offset-sm-1 col-md-8 offset-md-2 col-lg-8 offset-lg-2 text-sm-left text-md-left">
+            <header>
+                <h2 class="title text-center pt-2 pb-3" style="letter-spacing: 4.5px; padding-left: 4.7%;">GIVE US A CALL: <span class="d-xs-block d-sm-block d-md-block d-lg-inline title-header text-center"style="padding-top: 10px;">1-800-972-3981</span></h2>
+            </header>
             <div class="row">
                 <div class ="col-xs-12 col-sm-4 col-md-4 col-lg-2">
                     <h6 class="text-uppercase heads fonts font-weight-custom blue py-4">Why ibinex</h6>
